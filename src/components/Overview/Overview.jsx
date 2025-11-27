4c6f1c7c562bd65bb0832be1129316f6d2abe388
OlehKotov <mail.oleg456@gmail.com>
2025-11-27 18:06:21 +0200

    Update Overview.jsx

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -15,20 +15,20 @@ const Overview = () => {
   const apartmentId = useSelector(selectUserApartmentId);
 
   useEffect(() => {
-    if (apartmentId) {
+    if (apartmentId && !apartment) {
       dispatch(fetchApartmentByApartmentId(apartmentId));
     }
-  }, [dispatch, apartmentId]);
+  }, [dispatch, apartmentId, apartment]);
 
   return (
     <div className={css.container}>
       <h2 className={css.header}>My Apartment Info</h2>
       <ul className={css.list}>
-        <li className={css.item}>Floor - {apartment.floor}</li>
+        <li className={css.item}>Floor - {apartment?.floor}</li>
         <li className={css.item}>
-          Apartment Number - {apartment.apartmentNumber}
+          Apartment Number - {apartment?.apartmentNumber}
         </li>
-        <li className={css.item}>Area - {apartment.squareMeters}</li>
+        <li className={css.item}>Area - {apartment?.squareMeters}</li>
         <li className={css.item}>Owner - {userName}</li>
       </ul>
     </div>
